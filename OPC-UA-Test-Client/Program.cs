5c9b8d8aada859abfa5ea7c7c995a3eb45f21515
Steven Pennington <66976568+spennington45@users.noreply.github.com>
2023-11-08 08:34:58 -0600

    Add files via upload

---
+++ OPC-UA-Test-Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using TitaniumAS.Opc.Client;
+using TitaniumAS.Opc.Client.Common;
+using TitaniumAS.Opc.Client.Da;
+
+Console.WriteLine("Hello, World!");
+
+Bootstrap.Initialize();
+var url = UrlBuilder.Build("opc.tcp://127.0.0.1:4840/opcua/");
+
+using (var server = new OpcDaServer(url))
+{
+    server.Connect();
+}
+
+Console.ReadLine();
